8e821b6154137d201ebfef4f314064a717897644
Hugo NICOLAS-HUMBERT <hugo.nicolas-humbert@akanea.com>
2022-01-30 21:06:12 +0100

    Model + View + VM

---
+++ Perudo_UI/Model/Call.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perudo_UI.Model
+{
+    public class Call
+    {
+        public string KeyWord { get; set; }
+    }
+}
---
+++ Perudo_UI/ViewModel/GameViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Perudo_UI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perudo_UI.ViewModel
+{
+    public class GameViewModel
+    {
+        public ConnectionServer Server { get; set; }
+    }
+}
